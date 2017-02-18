15ae868a99cd86cc623bd83a09072ae1fcb06127
Egg0 <marcuslr@live.com>
2017-02-18 14:41:17 -0800

    rotating working sorta

---
+++ Bakk rikk/Assets/RotatePlatform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatform : MonoBehaviour {
+    public Quaternion rotation;
+    public float rotateIntensity = 20;
+	// Use this for initialization
+	void Start () {
+       
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(rotateIntensity, 0, 0) * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(-rotateIntensity, 0, 0) * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(0, 0, rotateIntensity) * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(0, 0, -rotateIntensity) * Time.deltaTime, Space.World);
+        }
+
+    }
+}
