97f89b74dc39c2caf5fe7d50bd39318b1ff0c195
Egg0 <marcuslr@live.com>
2017-02-18 21:06:05 -0800

    lifetracker wip

---
+++ Bakk rikk/Assets/Scripts/LifeTrackerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeTrackerScript : MonoBehaviour {
+    public static int lives;
+
+    // Use this for initialization
+    void Start () {
+        lives = 3;
+	}
+	
+	// Update is called once per frame
+	void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+}
