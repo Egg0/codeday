d420bca74d526e5f763ce3ef272a4db5859009e3
Egg0 <marcuslr@live.com>
2017-02-19 03:17:06 -0800

    lime lv 1

--- Bakk rikk/Assets/Scripts/CameraSwitch.cs
+++ Bakk rikk/Assets/Scripts/CameraSwitch.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CameraSwitch : MonoBehaviour {
+    public Camera cam1;
+    public Camera cam2;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cam1.enabled = !cam1.enabled;
+        cam2.enabled = !cam2.enabled;
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
