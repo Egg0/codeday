b01abc768b400478292dd215abceb9b9b1cf3e79
Vilfiera <kevong@uw.edu>
2017-02-19 01:05:06 -0800

    Completed Watermelon World

---
+++ Bakk rikk/Assets/Scripts/SpawnManyBalls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManyBalls : MonoBehaviour
+{
+    public int control;
+    public GameObject ball;
+    // Use this for initialization
+    void Update()
+    {
+        control++;
+        if (control % 15 == 0)
+        Instantiate(ball);
+
+    }
+
+}
