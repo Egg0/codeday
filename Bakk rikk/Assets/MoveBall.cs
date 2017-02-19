630545e8e3b729282de9a5f86496568734fc80db
Egg0 <marcuslr@live.com>
2017-02-18 17:57:34 -0800

    Ball rolling

--- Bakk rikk/Assets/MoveBall.cs
+++ Bakk rikk/Assets/MoveBall.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class RotatePlatform : MonoBehaviour {
+public class MoveBall : MonoBehaviour
+{
     public float speed;
+    public float maxVelocity = 500;
     private Rigidbody self;
-	
+
+
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
         self = GetComponent<Rigidbody>();
-	}
+    }
 
     // Update is called once per frame
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         //Vector: (Left/Right, 0, Forward/backwards) 
         int x = 0;
         int z = 0;
@@ -21,6 +27,7 @@ public class RotatePlatform : MonoBehaviour {
         if (Input.GetKey(KeyCode.S)) z = -1;
         if (Input.GetKey(KeyCode.D)) x = 1;
 
-        self.AddForce(new Vector3(x, 0, z) * speed);
+        if (Math.Abs(self.velocity.x) < maxVelocity) self.AddForce(new Vector3(x, 0, 0) * speed);
+        if (Math.Abs(self.velocity.z) < maxVelocity) self.AddForce(new Vector3(0, 0, z) * speed);
     }
 }
